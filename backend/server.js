36b83fe8da0b3ceeb3b4630e1dc563d3da5bec82
AnderBurd <AndersonLee0248@gmail.com>
2025-12-15 14:13:38 -0800

    Set up basic express structure

---
+++ backend/server.js
@@ -0,0 +1,37 @@
+//Env file
+require('dotenv').config();
+
+const express = require('express');
+const pool = require('pg');
+
+const app = express();
+const PORT = process.env.PORT || 3000; //3000 for local development
+
+const pool = new pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+//test route
+app.get('/', (req, res) => {
+    res.send("backend works");
+})
+
+//test database
+app.get('/db-test', async (req, res) => {
+    try {
+        await pool.query('SELECT NOW()');
+        res.send('Database connection successful!');
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Database connection failed.');
+    }
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server running on port ${PORT}`);
+
+});
