675d9b3803fe59e98208ba717d431dcbb413824f
AnderBurd <AndersonLee0248@gmail.com>
2025-12-26 13:04:35 -0800

    Added twitch player embed, with onTimeSelect functionality when clicking on the graph

--- frontend/src/screens/ViewHighlights.jsx
+++ frontend/src/screens/ViewHighlights.jsx
@@ -9,6 +9,7 @@ export default function ViewHighlights(){
     const {vodId} = useParams(); //vodId is in the url
     const [loading, setLoading] = useState(true);
     const [chartData,setChartData] = useState(null);
+    const [timeStamp, setTimeStamp] = useState("0h:00m:00s")
     
     //Fetch the data by calling our endpoint to find spikes
     useEffect( () =>{
@@ -26,14 +27,18 @@ export default function ViewHighlights(){
        fetchData();
     }, [vodId])
 
+    {/*Just sets the timestamp of the video player when clicking on a point on the graph*/}
     const onTimeSelect = (time) => {
-        console.log(time);
+        setTimeStamp(time);
     }
+
     if (loading) return <div>Loading...</div>;
     if (!chartData) return <div>No data</div>;
 
     return(
         <div className="Hype-wrapper">
+        {/*Twitch embed*/}
+        <iframe src={`https://player.twitch.tv/?video=${vodId}&time=${timeStamp}&parent=localhost`} frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe> 
         <HypeChart
             allData={chartData.allData}
             spikes={chartData.spikes}
